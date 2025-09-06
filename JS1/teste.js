32f20c09603a95fa44f5fb5286dab77f65d01863
LevelUp_Extreme <misakisonoda18@outlook.com>
2025-09-07 03:41:58 +0900

    VAI CORINTHIANS

--- JS1/teste.js
+++ JS1/teste.js
@@ -2,3 +2,37 @@ console.log("Ola, 'mundo'!");
 console.log('Ola, "mundo"!');
 console.log(`"Ola", 'mundo'!`);
 console.log(`Meu nome é "Misaki" e tenho`, 22, `anos!`);
+// comentario de uma linha
+/* comentario de
+muitas linhas
+*/
+/*let nome = "Misaki";
+let idade = 22;
+let usuario = "Carlos";
+console.log("Olá Mundo");
+        console.log("Console do navegador ");
+        //alert("Olá Mundo");
+        console.log("Olá ",nome);
+        console.log ("CAGUEI VEZES",idade)
+        console.log("SOU ",usuario);
+// Nao podemos criar Constantes/variaveis com palavras reservadas
+// Variaveis/Constantes precisam ter nomes significativos
+// Nao pode começar o nome de uma variavel/Constante com um numero    
+// Nao podem conter espacos ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Nao podemos redeclarar variaveis/Constantes com let
+// Nao utilize acentos e caracteres especiais
+// Utilizamos o padrao Unicode  
+// As variaveis/Constantes declaradas com let so existem no bloco onde foram criadas
+// Nao utilize var, utilize let (ES6 - 2015)
+// Nao utilize const, utilize let (ES6 - 2015)
+// Utilizamos const para valores que nao serao alterados
+*/
+let nome = "Misaki";
+let idade = 22;
+let peso = 57;
+let altura = 1.75;
+let imc = peso / (altura * altura);
+console.log("${nome} tem ${idade} anos e seu imc é de ${imc}");
+
