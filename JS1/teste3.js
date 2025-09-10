4fe51ce4858ec0cb4b875c2ce5e869a7db3ee722
LevelUp_Extreme <misakisonoda18@outlook.com>
2025-09-10 19:48:51 +0900

    ALGO

---
+++ JS1/teste3.js
@@ -0,0 +1,12 @@
+let num1 = 9.666775;
+num1 = Math.floor(num1); // arredonda para baixo  
+console.log(num1);
+
+let num2 = 9.666775;
+num2 = Math.ceil(num2); // arredonda para cima
+console.log(num2);
+console.log(Math.max(9, 5, 10, -2, 150, 3)); // retorna o maior valor
+console.log(Math.min(9, 5, 10, -2, 150, 3)); // retorna o menor valor
+
+console.log(Math.random()); // retorna um número aleatório entre 0 e 1
+console.log(Math.random() * (10 - 5) + 5); // retorna um número aleatório entre 5 e 10      
